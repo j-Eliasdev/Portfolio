269904a103755ffcaf32eb8a18283df16538a25b
Elias <j.batistadev@gmail.com>
2023-08-23 23:00:43 +0200

    btn-upDowm

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,12 @@
+import { BiUpArrowAlt } from "react-icons/bi";
+
 export default function FooterComponet() {
   return (
     <footer>
-      <div className="cr-con">Copyright &copy; 2023 | Made by JBatistaDev</div>
+      <div className="cr-con">
+        Copyright &copy; 2023 | Made by JBatistaDev
+       <a href="#home" > <BiUpArrowAlt /></a>
+      </div>
     </footer>
   );
 }
