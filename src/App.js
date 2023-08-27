0859733316ff74ae6bc840b02023af31187b14da
Elias <j.batistadev@gmail.com>
2023-08-27 04:46:08 +0200

    hoosk menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import NavarComponent from "./components/navbar";
-import FooterComponet from "./components/footer";
+import FooterComponent from "./components/footer";
 import LandingPage from "./views/landingPage";
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-      <FooterComponet />
+      <FooterComponent />
     </>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,34 @@
 import { BiUpArrowAlt } from "react-icons/bi";
+import { styled } from "styled-components";
 
-export default function FooterComponet() {
+export default function FooterComponent() {
   return (
-    <footer>
+    <Footer>
       <div className="cr-con">
         Copyright &copy; 2023 | Made by JBatistaDev
-       <a href="#home" > <BiUpArrowAlt /></a>
+        <a href="#home">
+          {" "}
+          <BiUpArrowAlt />
+        </a>
       </div>
-    </footer>
+    </Footer>
   );
 }
+
+const Footer = styled.footer`
+  .cr-con {
+    position: relative;
+
+    background-color: #122543;
+    color: white;
+    padding: 1rem 4rem;
+    text-align: center;
+  }
+  a {
+    position: absolute;
+    color: white;
+    font-size: x-large;
+    right: 1.5rem;
+    transition: 0.5s ease;
+  }
+`;
